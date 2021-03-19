5ef6ef647ad0d1877dc5bea3d07509c8485e760c
poojakota17 <poojaprasannan18@gmail.com>
2021-03-19 15:34:44 -0700

    aaadded cardview and recycler view to show park details

--- app/src/main/java/com/example/cmpe277/Searchactivity.kt
+++ app/src/main/java/com/example/cmpe277/Searchactivity.kt
@@ -1,7 +1,10 @@
 package com.example.cmpe277
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
@@ -16,6 +19,17 @@ class Searchactivity : AppCompatActivity() {
      val text= findViewById<EditText>(R.id.inputlocation)
       val  textvalue=text.text
         Toast.makeText( this,"location is $textvalue",Toast.LENGTH_LONG).show()
+        val intent = Intent(this, viewdetails::class.java ).apply{ putExtra(EXTRA_MESSAGE,textvalue ) }
+        startActivity(intent)
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+         super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            Toast.makeText( this,"Activity result received",Toast.LENGTH_LONG).show()
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/cmpe277/viewdetails.kt
+++ app/src/main/java/com/example/cmpe277/viewdetails.kt
@@ -2,10 +2,25 @@ package com.example.cmpe277
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class viewdetails : AppCompatActivity() {
+
+   // private var layoutManager: RecyclerView.LayoutManager? = null
+    //private var adapter: RecyclerView.Adapter<ParkListAdapter.ViewHolder>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_viewdetails)
+val myRecyclerView: RecyclerView= findViewById(R.id.myRecyclerView)
+     myRecyclerView.layoutManager= LinearLayoutManager(this)
+       val adapter:ParkListAdapter = ParkListAdapter()
+             // layoutManager= LinearLayoutManager(this)
+
+        //myRecylcerView.layoutManager = layoutManager
+      //  adapter= ParkListAdapter()
+        myRecyclerView.adapter=adapter
+
     }
 }
+
